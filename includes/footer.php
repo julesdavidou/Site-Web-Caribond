14eca29adc945b26f3755e8639f7d71d5c003893
Keyne <keyne.pekar@gmail.com>
2025-04-27 01:33:24 +0200

    Compartimentation includes

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,6 @@
 <footer>
     <p>&copy; 2025 Cari’Bond - Tous droits réservés</p>
 </footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-<script src="assets/js/particles.min.js"></script>
-<script src="assets/js/config_particules.js"></script>
+<?php include 'includes/common_js.php'; ?>
 </body>
 </html>
